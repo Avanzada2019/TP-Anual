0b1b564ae0bbddc38f1e7626263f774683226913
jbenitez <jbenitez@CGCSI020.casads.com.ar>
2019-06-14 15:42:06 -0300

    Creacion de menu en el Principal para luego realizar pruebas.

--- src/edu/usal/main/Principal.java
+++ src/edu/usal/main/Principal.java
@@ -1,11 +1,53 @@
 package edu.usal.main;
+import edu.usal.util.*;
 
 public class Principal {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		
 		System.out.println("Update de Javier 8/5/2019");
+		
+		
+		int opcionDeMenu=0;
+		
+		do {
+			
+			opcionDeMenu= IOGeneral.leerInt(null, null);
+			
+		}while (opcionDeMenu!= 0);
+		
+		do{
+			
+			switch (opcionDeMenu) {
+				case 1: // Gestion de Lineas Aereas
+						
+						
+					
+						break;
+	
+				case 2: // Gestion de Clientes
+					    
+					    break;
+				case 3: // Gestion de Ventas
+					    
+								
+					    break;
+				case 4: // Otras Gestiones
+										
+					    break;
+				case 5:
+					   System.out.println("Ha salido del sistema...");
+					   System.in.read(); // Queda esperando a que se presione una tecla.
+					   break;
+			} // Fin del switch
+			
+	
+		}while (opcionDeMenu!=6);  // Fin del menú General
+		
 
-	}
+   } //Cierre del Main
 
-}
+	
+	
+	
+} // Cierre del Principal.
