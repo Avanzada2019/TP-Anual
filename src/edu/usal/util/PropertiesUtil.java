dda32601fad8be05bd2c54b6ce8b81a1d017637e
Ezequ <Ezequ@LAPTOP-L15D6M56>
2019-06-17 16:47:52 -0300

    Properties modif

--- src/edu/usal/util/PropertiesUtil.java
+++ src/edu/usal/util/PropertiesUtil.java
@@ -8,6 +8,21 @@ import java.util.*;
 
 public class PropertiesUtil {
 	
+//EZE ---> DECLARACIÓN DE PROPERTY
+	private static Properties properties = new Properties();
+	private static PropertiesUtil objeto = null;
+	
+	private PropertiesUtil() throws FileNotFoundException , IOException
+	{
+		properties.load(new FileReader("config.properties"));
+	}
+	public static PropertiesUtil getInstance() throws FileNotFoundException, IOException
+	{
+		if (objeto == null)
+			objeto = new PropertiesUtil();
+		return objeto;
+	}
+//
 	
 	
 	public static String obtenerPathAerolineasStream() throws IOException, FileNotFoundException{
@@ -37,4 +52,51 @@ public class PropertiesUtil {
 		return properties.getProperty("pathClientesString");
 	}
 	
+	
+//Eze String
+	public static String obtenerPathAlianzas() throws FileNotFoundException, IOException{
+		Properties properties = new Properties();
+		return properties.getProperty("pathAlianzas");
+	}
+	
+	
+	public static String obtenerPathPaises()
+	{
+		Properties properties = new Properties();
+		return properties.getProperty("pathPaises");
+	}
+	
+	
+	public static String obtenerPathAeropuertos()
+	{
+		Properties properties = new Properties();
+		return properties.getProperty("pathAeropuertos");
+	}
+	
+	
+	public static String obtenerPathProvincias() throws FileNotFoundException, IOException
+	{
+		Properties properties = new Properties();
+		return properties.getProperty("pathProvincias");
+	}
+// 
+// Eze STREAM
+	public static String obtenerPathAeropuertosStream() throws IOException, FileNotFoundException{
+		Properties properties = new Properties();
+		properties.load(new FileReader("config.properties"));
+		
+		return properties.getProperty("pathAeropuertosStream");
+	}
+	
+	
+	public static String getPropertyAeropuerto()
+	{
+		return properties.getProperty("pathAeropuerto");
+	}
+	
+	
+	public static String getPropertyLineaAerea()
+	{
+		return properties.getProperty("pathLineaAerea");
+	}
 }
