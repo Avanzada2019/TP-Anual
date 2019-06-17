b0c712df2aa6f75465940e6a4a5271a40be7dcb9
Ezequ <Ezequ@LAPTOP-L15D6M56>
2019-06-17 19:32:10 -0300

    Correccion en Clase Venta, su implementacion y factory

--- src/edu/usal/negocio/dao/factory/VentasDAOFactory.java
+++ src/edu/usal/negocio/dao/factory/VentasDAOFactory.java
@@ -1,12 +1,14 @@
 package edu.usal.negocio.dao.factory;
 
+import java.util.stream.Stream;
+
 import edu.usal.negocio.dao.implementacion.VentasDAOImpStream;
 import edu.usal.negocio.dao.interfaces.VentaDAO;
 
 public class VentasDAOFactory {
-	public static VentaDAO obtenerVentaDAO(String tipo)
+	public static VentaDAO obtenerVentaDAO(Stream tipo)
 	{
-		if("String".equals(tipo))
+		if("Stream".equals(tipo))
 			return new VentasDAOImpStream();
 		return null;
 	}
