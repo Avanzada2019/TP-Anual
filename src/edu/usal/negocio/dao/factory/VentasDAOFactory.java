a00cdfdc3712599108c480a8f0db44d0bca0a8a8
Ezequ <Ezequ@LAPTOP-L15D6M56>
2019-06-17 18:15:20 -0300

    Creacion Factory de Ventas

--- src/edu/usal/negocio/dao/factory/VentasDAOFactory.java
+++ src/edu/usal/negocio/dao/factory/VentasDAOFactory.java
@@ -1,5 +1,13 @@
 package edu.usal.negocio.dao.factory;
 
-public class VentasDAOFactory {
+import edu.usal.negocio.dao.implementacion.VentasDAOImpStream;
+import edu.usal.negocio.dao.interfaces.VentaDAO;
 
+public class VentasDAOFactory {
+	public static VentaDAO obtenerVentaDAO(String tipo)
+	{
+		if("String".equals(tipo))
+			return new VentasDAOImpStream();
+		return null;
+	}
 }
