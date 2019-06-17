afa8fe36a2ff0914a581ecb0cfa5bad47b3f2544
Ezequ <Ezequ@LAPTOP-L15D6M56>
2019-06-17 17:12:05 -0300

    interfaz basica

--- src/edu/usal/negocio/dao/implementacion/VuelosDAOImpStream.java
+++ src/edu/usal/negocio/dao/implementacion/VuelosDAOImpStream.java
@@ -1,5 +1,36 @@
 package edu.usal.negocio.dao.implementacion;
 
-public class VuelosDAOImpStream {
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.List;
+
+import edu.usal.negocio.dao.interfaces.VentaDAO;
+import edu.usal.negocio.dominio.Venta;
+
+public class VuelosDAOImpStream implements VentaDAO {
+
+	@Override
+	public List<Venta> obtenerVenta() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void altaVenta(Venta AltaVenta) throws FileNotFoundException, IOException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void modificarVenta(Venta ModificarVenta) throws FileNotFoundException, IOException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void bajaVenta(Venta BajarVenta) throws FileNotFoundException, IOException {
+		// TODO Auto-generated method stub
+
+	}
 
 }
