2f9b805fb40549c0289ef2791f4e1cfaa8b74a6f
sergiobaltazar <49718970+sergiobaltazar@users.noreply.github.com>
2019-06-17 17:35:08 -0300

    Agregado de impl vuelos

---
+++ src/edu/usal/negocio/dao/implementacion/VueloDAOImpStream.java
@@ -0,0 +1,37 @@
+package edu.usal.negocio.dao.implementacion;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.List;
+
+import edu.usal.negocio.dao.interfaces.VueloDAO;
+import edu.usal.negocio.dominio.Vuelo;
+
+
+public class VueloDAOImpStream implements VueloDAO {
+
+	@Override
+	public List<Vuelo> obtenerVuelo() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void altaVuelo(Vuelo AltaVuelo) throws FileNotFoundException, IOException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void modificarVuelo(Vuelo ModificarVuelo) throws FileNotFoundException, IOException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void bajaVuelo(Vuelo BajarVuelo) throws FileNotFoundException, IOException {
+		// TODO Auto-generated method stub
+
+	}
+		
+}
