b35821c2066402a8fdd571e8a4f8f07441fb1d71
sergiobaltazar <49718970+sergiobaltazar@users.noreply.github.com>
2019-06-17 18:31:13 -0300

    Factory + IMplementacion

--- src/edu/usal/negocio/dao/factory/VuelosDAOFactory.java
+++ src/edu/usal/negocio/dao/factory/VuelosDAOFactory.java
@@ -1,7 +1,20 @@
 package edu.usal.negocio.dao.factory;
 
+import edu.usal.negocio.dao.interfaces.VueloDAO;
+import edu.usal.negocio.dao.implementacion.VueloDAOImpStream;
+import edu.usal.negocio.dao.implementacion.VueloDAOImpString;
+
 public class VuelosDAOFactory {
 
+	public static VueloDAO obtenerVueloDAO(String tipo)
+	{
+		if("String".equals(tipo))
+			return new VueloDAOImpString();
+		if("Stream".equals(tipo))
+			return new VueloDAOImpStream();
+		return null;
+	}
 	
-	//test
+	
+	
 }
--- src/edu/usal/negocio/dao/implementacion/VueloDAOImpStream.java
+++ src/edu/usal/negocio/dao/implementacion/VueloDAOImpStream.java
@@ -4,30 +4,31 @@ import java.io.EOFException;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 import java.util.List;
 
 import edu.usal.negocio.dao.interfaces.VueloDAO;
-import edu.usal.negocio.dominio.LineaAerea;
 import edu.usal.negocio.dominio.Vuelo;
 import edu.usal.util.PropertiesUtil;
 
-public class VueloDAOImpStream implements VueloDAO {
+public class VueloDAOImpStream implements VueloDAO{
 
 	@Override
-	public List<Vuelo> obtenerVuelo() {
-
-		List<Vuelo> Vuelo = new ArrayList<Vuelo>();
+	public List<Vuelo> obtenerVuelo(){
+	
+		List<Vuelo> aerolineas = new ArrayList<Vuelo>();
 		try
 		{
-			FileInputStream fis = new FileInputStream(new File(PropertiesUtil.obtenerPathVueloStream()));
+			FileInputStream fis = new FileInputStream(new File(PropertiesUtil.obtenerPathAerolineasStream()));
 			ObjectInputStream ois = new ObjectInputStream(fis);
 			
 			try
 			{
-				Vuelo = (List<Vuelo>) ois.readObject();	
+					aerolineas = (List<Vuelo>) ois.readObject();	
 			} 
 			catch (EOFException e) { }
 			ois.close();
@@ -37,27 +38,97 @@ public class VueloDAOImpStream implements VueloDAO {
 		{
 			e.printStackTrace();
 		}
-		return Vuelo;
-	}
+		return aerolineas;
+	} 
+	// Cierre de obtenerVuelo
+
 	
-	//Fin Listar Vuelo
+	@Override
+	public void modificarVuelo(Vuelo modificarVuelo) throws FileNotFoundException, IOException {
+				
+		List <Vuelo> listado = obtenerVuelo();
+		
+		for(Vuelo la : listado)
+		{
+			
+			if (la.getNombreAerolinea().equals(modificarVuelo.getNombreAerolinea()))
+			{
+				la.setNombreAerolinea(modificarVuelo.getNombreAerolinea());
+				
+				
+			}
+		}
+		
+		FileOutputStream ArchivoDeSalida = new FileOutputStream(PropertiesUtil.obtenerPathAerolineasStream());
+		ObjectOutputStream oArchivoDeSalida = new ObjectOutputStream(ArchivoDeSalida);
+		
+		oArchivoDeSalida.writeObject(listado);
+		oArchivoDeSalida.close(); 
+		
+	
+	}  // Cierre de modificarVuelo
+	
+public void bajaVuelo(Vuelo BajarVuelo) throws FileNotFoundException, IOException{
+		
+		List <Vuelo> listadoVuelo = obtenerVuelo(); // Cargo la todas las lineas en listadoVuelo
+		
+		for (Vuelo l : listadoVuelo) {
+		   if (l.getNombreAerolinea().equals(BajarVuelo.getNombreAerolinea())) {
+			   
+			   l.setNombreAerolinea(BajarVuelo.getNombreAerolinea());
+			   l.setAlianza(BajarVuelo.getAlianza());
+			   l.setVuelos(BajarVuelo.getVuelos());
+		   }
+		}
+		
+		FileOutputStream ArchivoDeSalida = new FileOutputStream(PropertiesUtil.obtenerPathAerolineasStream());
+		ObjectOutputStream oArchivoDeSalida = new ObjectOutputStream(ArchivoDeSalida);
+		
+		oArchivoDeSalida.writeObject(listadoVuelo);
+		oArchivoDeSalida.close();
+		
+		// oArchivoDeSalida = es el Objeto ArchivoDeSalida
+		
+	}  // Cierre Baja Vuelo
+
 
 	@Override
 	public void altaVuelo(Vuelo AltaVuelo) throws FileNotFoundException, IOException {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void modificarVuelo(Vuelo ModificarVuelo) throws FileNotFoundException, IOException {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void bajaVuelo(Vuelo BajarVuelo) throws FileNotFoundException, IOException {
-		// TODO Auto-generated method stub
-
-	}
 		
+		List <Vuelo> listadoVuelo = new ArrayList<Vuelo>();
+		
+		try
+		{
+			
+			
+			FileInputStream archivoDeEntrada = new FileInputStream(PropertiesUtil.obtenerPathAerolineasStream());
+			ObjectInputStream oArchivoDeEntrada = new ObjectInputStream(archivoDeEntrada);
+			
+			listadoVuelo = (List <Vuelo>) oArchivoDeEntrada.readObject();
+			
+			
+			listadoVuelo = (List <Vuelo>) oArchivoDeEntrada.readObject();
+			oArchivoDeEntrada.close();
+			
+		}
+		catch(Exception e)
+		{
+			System.out.println(e.getMessage());
+		}			
+				
+		FileOutputStream ArchivoDeSalida =  new FileOutputStream(PropertiesUtil.obtenerPathAerolineasStream());
+		ObjectOutputStream oArchivoDeSalida = new ObjectOutputStream(ArchivoDeSalida);
+		
+		listadoVuelo.add(AltaVuelo);
+        
+		oArchivoDeSalida.writeObject(listadoVuelo);
+		oArchivoDeSalida.close();
+		
+	};  // Cierre alta Vuelo
+	
+	
+	
+	
+	
+	
 }
